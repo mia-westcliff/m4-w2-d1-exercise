f24c5ca80eae0059713f3a6929e240c10bd7b835
mia-westcliff <143572221+mia-westcliff@users.noreply.github.com>
2023-11-27 19:45:35 -0800

    inclass changes

--- db/users.js
+++ db/users.js
@@ -1,5 +1,6 @@
 var records = [
-    // add users here
+  { id: 1, username: 'westcliff', password: 'secret', displayName: 'WestCliff', emails: [ { value: 'west@example.com' } ] }, 
+  { id: 2, username: 'westcliffclass', password: 'birthday', displayName: 'WestCliff University', emails: [ { value:'cliff@example.com' } ] }
 ];
 
 exports.findById = function(id, cb) {
